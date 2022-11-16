ac80b5c45bc7c21ca1d4a35c300ab79ad596c92e
marouaz2 <marouazebda@gmail.com>
2022-11-16 23:04:24 +0100

    react

---
+++ src/components/Warning.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../styles/Warning.css";
+
+function Warning() {
+  return (
+    <div className="Warning">
+      <h2> City not found </h2>
+      <p>Try again...</p>
+    </div>
+  );
+}
+
+export default Warning;
